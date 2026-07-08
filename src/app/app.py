4c974823377468af5bfdc9ced56944ea199d82ee
Arthur <chrolloacb@gmail.com>
2026-07-08 20:39:42 -0300

    Análise de Dados de candidatos políticos 2024

---
+++ src/app/app.py
@@ -0,0 +1,116 @@
+# %%
+import streamlit as st
+import pandas as pd
+import sqlalchemy
+import matplotlib.pyplot as plt
+import seaborn as sns
+import gdown
+import os
+from utils import make_scatter, make_clusters
+
+
+app_path = os.path.dirname(os.path.abspath(__file__))
+src_path = os.path.dirname(app_path)
+base_path = os.path.dirname(src_path)
+data_path = os.path.join(base_path, "data")
+
+
+@st.cache_data(ttl=60*60*24)
+def create_df():
+    filename = os.path.join(data_path, "data_partidos.parquet")
+    return pd.read_parquet(filename)
+
+# %%
+
+features_map = {
+    "PERCENTUAL FEMININO": "txGenFeminino",
+    "PERCENTUAL RACA PRETA": "txCorRacaPreta",
+    "PERCENTUAL RACA PRETA PARDA": "txCorRacaPretaParda",
+    "PERCENTUAL RACA NAO-BRANCA": "txCorRacaNaoBranca",
+    "MEDIA BENS TOTAL": "avgBens",
+    "MEDIA BENS SEM ZEROS": "avgBensNotZero",
+    "PERCENTUAL ESTADO CIVIL CASADO(A)": "txEstadoCivilCasado",
+    "PERCENTUAL ESTADO CIVIL SOLTEIRO(A)": "txEstadoCivilSolteiro",
+    "PERCENTUAL ESTADO CIVIL DIVORCIADO(A)": "txEstadoCivilSeparadoDivorciado",
+    "MEDIA IDADE": "avgIdade",
+}
+
+features_options = list(features_map.keys())
+features_options.sort()
+
+# %%
+df = create_df()
+
+welcome = """
+# TSE Analytics - Eleições 2024
+
+Altere as opções dos botões abaixo para ver as mudanças no gráfico.    
+"""
+
+st.markdown(welcome)
+
+uf_options = df["SG_UF"].unique().tolist()
+uf_options.sort()
+uf_options.remove("BR")
+uf_options = ["BR"] + uf_options
+
+cargos_options = df["DS_CARGO"].unique().tolist()
+cargos_options.sort()
+cargos_options.remove("GERAL")
+cargos_options = ["GERAL"] + cargos_options
+
+## Definição do estado e cargo
+col1, col2 = st.columns(2)
+with col1:
+    estado = st.selectbox(label="Estado", placeholder="Selecione o estado para filtro", options=uf_options)
+    
+with col2:
+    cargo = st.selectbox(label="Cargo", placeholder="Selecione o cargo para filtro", options=cargos_options)
+
+## Definição dos eixos
+col1, col2 = st.columns(2)
+with col1:
+    x_option = st.selectbox(label="Eixo x", options=features_options, index=6)
+    x = features_map(x_option)
+    new_features_options = features_options.copy()
+    new_features_options.remove(x_option)
+
+with col2:
+    y_option = st.selectbox(label="Eixo y", options=new_features_options, index=7)
+    y = features_map(y_option)
+
+size = st.checkbox("Tamanho das bolhas")
+
+## Definição do uso de cluster
+col1, col2, = st.columns(2)    
+with col1:
+    cluster = st.checkbox("Definir cluster")
+    if cluster:
+        n_cluster = st.number_input("Quantidade de clusters", value=6, format="%d", max_value=10, min_value=1)
+
+with col2:
+    if cluster:
+        features_options_selected = st.multiselect(label="Variáveis para agrupamento", options=features_options, default=["PERCENTUAL FEMININO", "PERCENTUAL RACA PRETA"])
+        features_selected = [features_map[i] for i in features_options_selected]
+
+
+data = df[(df["SG_UF"]==estado) & (df["DS_CARGO"]==cargo)].copy()
+
+total_candidatos = int(data["totalCandidaturas"].sum())
+st.markdown(f"Total de candidaturas: {total_candidatos}")
+
+if cluster:
+    data = make_clusters(data=data, features=features_selected, n=n_cluster)
+
+fig = make_scatter(data, 
+                   x=x, y=y,
+                   x_label=x_option, y_label=y_option, 
+                   cluster=cluster, 
+                   size=size)
+
+st.pyplot(fig)
+
+st.markdown("""
+### Créditos
+Você pode conferir o repositório deste projeto aqui: 
+            """)
---
+++ src/app/utils.py
@@ -0,0 +1,87 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from sklearn import cluster
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+from adjustText import adjust_text
+from sklearn import preprocessing
+
+def make_scatter(data,x, y, x_label, y_label, cluster=False, size=False):
+
+    config = {
+        "data":data, 
+        "x":x, 
+        "y":y,
+        "size":"totalCandidaturas",
+        "sizes":(5,300),
+        "hue":"cluster",
+        "palette":'viridis',
+        "alpha":0.6
+    }
+
+    if not cluster:
+        del config['hue']
+        del config ["palette"]
+
+    if not size:
+        del config['size']
+        del config['sizes']
+
+    fig = plt.figure(dpi=360, figsize=(6,5.5))
+
+    sns.scatterplot(**config)
+
+    texts = []
+    for i in data['SG_PARTIDO']:
+        data_tmp = data[data['SG_PARTIDO']==i]
+        x_pos = data_tmp[x].values[0]
+        y_pos = data_tmp[y].values[0]
+        texts.append(plt.text(x_pos, y_pos, i, fontsize=9))
+
+    adjust_text(texts, force_poins=0.0002,
+            force_text=0.4,
+            expand_points=(0.5, 0.75), expand_text=(0.5, 0.75),
+            arrowprops=dict(arrowstyle="-", color='black', lw=0.2),
+            pull_threshold=1000,)
+
+    plt.grid(True)
+    plt.suptitle(f"Partidos: {x_label.title()} vs {y_label.title()} - Eleições 2024")
+    
+    if size:
+        plt.title("Maior a bolha, maior o tamanho do partido", fontdict={"size":9})
+    
+    plt.xlabel(x_label.title())
+    plt.ylabel(y_label.title())
+
+    if x == 'txGenFeminino' and y == 'txCorRacaPreta':
+        txCorRacaPreta = data['totalCorRacaPreta'].sum() / data['totalCandidaturas'].sum()
+        txGenFeminino = data['totalGenFeminino'].sum() / data['totalCandidaturas'].sum()
+
+
+        plt.hlines(y=txCorRacaPreta, 
+                  xmin=data['txGenFeminino'].min(), 
+                  xmax=data['txGenFeminino'].max(), colors="black", alpha=0.6, linestyles="--", label=f"Pessoas Pretas Geral: {100*txCorRacaPreta:.0f}%")
+
+        plt.vlines(x=txGenFeminino, 
+                  ymin=data['txCorRacaPreta'].min(), 
+                   ymax=data['txCorRacaPreta'].max(), colors="tomato", alpha=0.6, linestyles="--", label=f"Mulheres Geral: {100*txGenFeminino:.0f}%")
+
+        handles, labels = plt.gca().get_legend_handles_labels()
+        handles = handles[-2:]
+        labels = labels[-2:]
+
+        plt.legend(handles=handles, labels=labels)
+
+    else:
+        legend = plt.legend()
+        legend.remove()
+
+    return fig
+
+def make_clusters(data, features, n=6):
+    norm = preprocessing.MinMaxScaler()
+    data_norm = norm.fit_transform(data[features])
+    model = cluster.KMeans(n_clusters=n, random_state=42, max_iter=10000)
+    model.fit(data_norm)
+    data['cluster'] = model.labels_
+    return data
